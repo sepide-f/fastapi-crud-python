c4ec69c8899f48242795d447069761d63c0c0cb6
sepide <135548271+sepide-f@users.noreply.github.com>
2024-08-14 10:57:26 +0330

    Update auth.py

--- user-api/auth.py
+++ user-api/auth.py
@@ -1,1 +1,42 @@
+from passlib.context import CryptContext
+from datetime import datetime, timedelta
+from typing import Union
+from jose import JWTError, jwt
+from fastapi import status, Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+
+SECRET_KEY = "3bbdc4dc8922cd91a7fc31a9b6b82f9c0ebf365f10f73c6406b1eb2c9431d364"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None) -> str:
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+def hash_password(password: str) -> str:
+    return pwd_context.hash(password)
+
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_current_user_role(token: str = Depends(oauth2_scheme)) -> str:
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload.get("role", "user")  # Default to "user" if no role is found
+    except JWTError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
 
